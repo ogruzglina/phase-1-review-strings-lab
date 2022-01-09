87e50210d2228bb1ea9bf46826d21361f4e67006
Olha Gruzglina <olga.zhmura@gmail.com>
2022-01-09 16:32:25 -0500

    commit tree 5c5a651eeebc3d127075374bb099947fe6a273d2

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+let currentUser = 'Olha Gruzglina';
+let welcomeMessage = 'Welcome to Flatbook, ';
+welcomeMessage += currentUser + '!';
+console.log(welcomeMessage);
+
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+console.log(excitedWelcomeMessage);
+
+let shortGreeting = `Welcome, ${currentUser[0]}!`;
+console.log(shortGreeting);
